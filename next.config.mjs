5b3dd55732e02ff8de06e56cc382c762ecba4626
Tian Sheng <siewtiansheng@gmail.com>
2024-05-21 13:58:08 +0800

    update build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  assetPrefix: "https://sagorzaii.github.io/berapa-calculator/",
-  // basePath: "/berapa-calculator",
-};
+const nextConfig = {};
 
 export default nextConfig;
